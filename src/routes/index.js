f27cbb808ebd38b283a9206de73e0bcd15ea054f
jemosdev <jerson.mosquera.p@gmail.com>
2023-04-16 18:19:55 -0500

    creating routes

---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+import Header from '../templates/Header';
+import Home from '../pages/Home';
+import Character from '../pages/Character';
+import Error404 from '../pages/Error404';
+
+//routes of app
+const routes = {
+    '/': Home,
+    '/:id': Character,
+    '/contact': 'Contact',
+};
+
+//Router - handler to show elements based on the logic
+const router = async () => {
+    const header = null || document.getElementById('header');
+    const content = null || document.getElementById('content');
+
+    header.innerHTML = await Header();
+};
+
+export default router;
