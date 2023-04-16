9ea4f76abf3af8dc58e31f293a324f790ae1685e
jemosdev <jerson.mosquera.p@gmail.com>
2023-04-15 23:40:18 -0500

    creating error404 template

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,32 @@
-//allow to access where we are in directory
 const path = require('path'); 
-//file or plugin to work with HTML
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-//all the setting to export
 module.exports = {
-    entry: './src/index.js', //entry point where live the code
+    entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'), //send the project to production, creating the directory called dist
-        filename: 'main.js' //final name to production
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
     },
     resolve: {
-        extensions: ['.js'] //extensions to use
+        extensions: ['.js']
     },
     module: {
-        rules: [//module with rules to use
+        rules: [
             {
-                test: /\.js?$/,//allow identify the files in the place
-                exclude: /node_modules/,//exclude node_module directory
+                test: /\.js?$/,
+                exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader'//use a loader to work
+                    loader: 'babel-loader'
                 }
             }
         ]
     },
-    plugins: [//plugins to use
-        new HtmlWebpackPlugin([//allow to work with HTML files
+    plugins: [
+        new HtmlWebpackPlugin([
             {
-                inject: true,//how to inject a value to HTML file
-                template: './public/index.html',//path where the main template is
-                filename: './index.html'//name that will have the file
+                inject: true,
+                template: './public/index.html',
+                filename: './index.html'
             }
         ])
     ]
